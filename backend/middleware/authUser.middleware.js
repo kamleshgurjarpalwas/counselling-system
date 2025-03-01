94901d815e879ec5e6b5ae88e1e63a71a492783a
Kamlesh Gurjar <kamleshgurjarpalwas@gmail.com>
2025-03-01 13:53:32 +0530

    This is testing

--- backend/middleware/authUser.middleware.js
+++ backend/middleware/authUser.middleware.js
@@ -2,10 +2,16 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-module.exports.authUser = async function (req,res) {
+module.exports.authUser = async function (req, res,next) {
   const token = req.cookie.token || req.cookie.authorization?.splite(" ")[1];
 
-  if(!token){
-    res.
+  if (!token) {
+    res.status(400).json({ message: "unauthorized user" });
   }
-}
+
+  try{
+
+  }catch{
+    
+  }
+};
