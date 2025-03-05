448d77ce33955054a161acbad69bb70e19ae209a
DJ-InfinityCoder <161459648+DJ-InfinityCoder@users.noreply.github.com>
2025-03-05 17:08:41 +0530

    conflict remove after frontend of college updated

--- backend/services/users.service.js
+++ backend/services/users.service.js
@@ -5,7 +5,6 @@ module.exports.createUser = async function ({ name, email, password }) {
   return user;
 };
 
-//This is for registering user
 module.exports.register = async function ({ isUserExist, hashedPassword }) {
   isUserExist.password = hashedPassword;
   isUserExist.isRegistered = true;
